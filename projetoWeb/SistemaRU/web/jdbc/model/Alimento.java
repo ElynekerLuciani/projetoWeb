e4a33009176a269fef612f1eb0f4c0a66d3fafd7
Marcelo <Marcelo@DESKTOP-765PDC1>
2018-04-13 21:44:41 -0300

    Adição da classe Alimento

---
+++ projetoWeb/SistemaRU/web/jdbc/model/Alimento.java
@@ -0,0 +1,62 @@
+package web.jdbc.model;
+
+public class Alimento implements InterfaceManipulable{
+	private int id;
+	private String nome;
+	private CategoriaAlimento categoria;
+	
+	
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void find(int codigo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		if(id >= 0) {
+			this.id = id;
+		}
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		if(nome != null) {
+			this.nome = nome;
+		}
+	}
+
+	public CategoriaAlimento getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(CategoriaAlimento categoria) {
+		if(categoria != null) {
+			this.categoria = categoria;
+		}
+	}
+}
